4d62f593e3b6fb9669cb0f32141d2c34b25d96f0
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2018-08-17 11:24:59 +0100

    added separate folder I used for trying to make a web app

---
+++ Web App Practice/Bingo_Project/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Bingo_Project.Models;
+
+namespace Bingo_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Your index page.";
+            return View();
+        }
+
+        public IActionResult Bingo_Project()
+        {
+            ViewData["Message"] = "Your bingo_project page.";
+           return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
