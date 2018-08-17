35be846c7c56fcc58013a38f55efb39aa6226b7f
Tech-A34 <Tech-A34@Tech-A34s-MacBook-Air.local>
2018-08-17 14:51:03 +0100

    added number select

--- Bingo1/Bingo.cs
+++ Bingo1/Bingo.cs
@@ -7,14 +7,31 @@ namespace Bingo1
         static void Main(string[] args)
         {
 
-            // Create array list of 1 to 100 START
+            int number_of_balls = 75;
+            int[] bingoCard = new int[6];
+
+            for (int i = 0; i < bingoCard.Length; i++)
+            {
+                Console.Write("Select your {0}th number. ", i+1);
+                bingoCard[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("You have selected the following numbers:");
+
+            for (int i = 0; i < bingoCard.Length; i++)
+            {
+                Console.WriteLine(bingoCard[i]);
+            }    
+
+
+            // Create array list of 1 to 75 START
             ArrayList remaining_numbers = new ArrayList();
 
-            for (int i = 1; i < 101; i++)
+            for (int i = 1; i < number_of_balls+1; i++)
             {
                 remaining_numbers.Add(i);
             }
-            // Create array list of 1 to 100 END
+            // Create array list of 1 to 75 END
 
 
             // Uncomment this if you want to check that the numbers 1 to 100 are in the list array
@@ -24,14 +41,14 @@ namespace Bingo1
             //}
 
 
-            // There will be 100 rounds - a round for each bingo ball
-            //int rounds = remaining_numbers.Count;
+            // There will be 75 rounds - a round for each bingo ball
+            int rounds = remaining_numbers.Count;
 
 
             ArrayList chosen_nums = new ArrayList();
 
             // This loop removes one number from the list array for each round
-            for (int i = 0; i < 25; i++)
+            for (int i = 0; i < rounds; i++)
             {
                 // Randomly selects one of the remaining numbers
                 Random rnd = new Random();
@@ -39,7 +56,7 @@ namespace Bingo1
 
                 Console.WriteLine("The {0}th number is {1}", i + 1, remaining_numbers[next_num_index]);
 
-                chosen_nums.Add(remaining_numbers[next_num_index])
+                chosen_nums.Add(remaining_numbers[next_num_index]);
 
 
                 // Removes the randomly selected number
---
+++ Bingo1/MakeBingoCard.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//namespace Bingo1
+//{
+//    public class MakeBingoCard
+//    {
+
+//        int[] bingoCard = new int[5];
+
+//        for (int i = 0; i < bingoCard.Count; i++)
+//        {
+//             bingoCard.Add(i);
+//        }
+
+//        for (int i = 0; i < bingoCard.Count; i++);
+//        {
+//            Console.Write(bingoCard[i]);
+//        }
+
+
+
+
+//    }
+//}
